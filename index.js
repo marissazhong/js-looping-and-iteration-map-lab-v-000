9e7e24b37b5e81fed386beba8d4887f72ef007da
Marissa Zhong <mwz2101@columbia.edu>
2018-07-16 00:34:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(drivers) {
+  return drivers.map(x => x.toLowerCase());
+}
+
+function nameToAttributes(names) {
+  const result = [];
+  names.map(function (x) {
+    let first_and_last = x.split(" ");
+    result.push({firstName: first_and_last[0], lastName: first_and_last[1]});
+  })
+  return result;
+}
+
+function attributesToPhrase(names) {
+  const result = [];
+  names.map(function (x) {
+    result.push(`${x.name} is from ${x.hometown}`);
+  })
+  return result;
+}
