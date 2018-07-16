b7a968b52bae89becaef2b6099de711fcf46ecb1
Marissa Zhong <mwz2101@columbia.edu>
2018-07-16 00:26:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ function lowerCaseDrivers(drivers) {
 
 function nameToAttributes(names) {
   const result = {};
-  
-}
+  names.map(function (x) {
+    let first_and_last = x.split(" ");
+    result.firstName = first_and_last[0];
+    result.lastName = first_and_last[1];
+  })
