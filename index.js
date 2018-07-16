d8e38808c61793d243bc5a037b3f3d7a0e4d534a
Marissa Zhong <mwz2101@columbia.edu>
2018-07-16 00:27:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ function nameToAttributes(names) {
   const result = {};
   names.map(function (x) {
     let first_and_last = x.split(" ");
+    console.log(first_and_last);
     result.firstName = first_and_last[0];
     result.lastName = first_and_last[1];
   }
   return result;
 }
+
+const drivers = ['Bobby Smith', 'Sammy Watkins', 'Sally Jenkins', 'Annette Sawyer', 'Sarah Hucklebee', 'bobby ridge'];
+nameToAttributes(drivers);
