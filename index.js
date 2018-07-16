1d35d235dfbfa9436696358e82fb2909eeab063a
Marissa Zhong <mwz2101@columbia.edu>
2018-07-16 00:29:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ function lowerCaseDrivers(drivers) {
 }
 
 function nameToAttributes(names) {
-  const result = {};
+  const result = [];
   names.map(function (x) {
     let first_and_last = x.split(" ");
-    result.firstName = first_and_last[0];
-    result.lastName = first_and_last[1];
+    result.push({firstName: first_and_last[0], lastName: first_and_last[1]});
   })
   return result;
 }
